10e75eb410f7dd668fa3dc7d5633018ac9931d08
Afreda323 <anthonyfreda323@gmail.com>
2018-08-01 22:27:21 -0400

    added logger

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello world")
+const express = require('express')
+const logger = require('./logger')
+
+const app = express()
+
+app.listen(1234, () => {
+  logger.error('App up on 1234')
+})
